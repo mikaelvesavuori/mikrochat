5c91374011c9deb7c3382d24c27f73f2be313508
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2025-04-04 08:58:23 +0200

    fix: release script

--- release.sh
+++ release.sh
@@ -6,5 +6,5 @@ mkdir releases
 npm run build
 
 zip -r releases/mikrochat.app.zip dist
-zip releases/mikrochat.bundled.zip lib/mikrochat.bundled.cjs
-zip releases/mikrochat.bundled.zip lib/mikrochat.bundled.mjs
+zip releases/mikrochat.bundled.cjs.zip lib/mikrochat.bundled.cjs
+zip releases/mikrochat.bundled.mjs.zip lib/mikrochat.bundled.mjs
